d22fcaa0321e3dae6f3ff2049e560722eab4119e
Jarbas Martins <jarbasmartins0708@gmail.com>
2025-11-07 10:51:36 -0300

    update misc project files

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import "./style.css";
+
+import { searchButton } from "./script/Ui/DomElements";
+import { initWeather } from "./script/services";
+import { getCityStats } from "./script/services/forecast.service";
+import type { SelectedCity } from "./types/Forecast";
+import { updateCityDisplay } from "./script/Ui/weatherDisplay";
+
+searchButton?.addEventListener("click", (e) => {
+    e.preventDefault();
+    initWeather();
+});
+
+window.addEventListener("load", () => {
+    const lastRequest = localStorage.getItem("LastRequest");
+    if (!lastRequest) return;
+    const parsed: SelectedCity = JSON.parse(lastRequest);
+    getCityStats(parsed.latitude, parsed.longitude);
+    updateCityDisplay(parsed);
+});
